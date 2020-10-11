e2bef6c78d44ccaa319f9d0e3574e0054663a87c
nm7uh <nm7uh@mathcs02.richmond.edu>
2020-10-11 17:27:35 -0400

    Implemented all methods except append

--- CharacterVector.cpp
+++ CharacterVector.cpp
@@ -10,25 +10,34 @@ CharacterVector::~CharacterVector() {}
 
 int CharacterVector::size() 
 {  
-   return ...
+   return characterVector.size();
 }
 
 // just return the character at the specified index; use the at() method rather
 // than [] so that an out_of_range error will be thrown for an invalid index
 char CharacterVector::get(int index) 
 { 
-   return ...
+   return characterVector.at(index);
 }
 
 // if index is size-legitimate, put the value at that index;
 // otherwise, use push_back to append to the end of the vector
 void CharacterVector::put(char value, int index)
 {
+   if (index < 0 || (unsigned)index >= characterVector.size())
+   {
+      characterVector.push_back(value);
+   } else
+   {
+      std::vector<char>::iterator it = characterVector.begin();
+      characterVector.insert(it + index, value);
+   }
 }
 
 // use push_back to append
 void CharacterVector::put(char value)
 {
+   characterVector.push_back(value);
 }
 
 // for each double in doubleVector, use static_cast<char> to append as a
