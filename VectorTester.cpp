7bc7ff41582c8b850aa2916be23941aea913f273
hj9kg <hj9kg@mathcs01.richmond.edu>
2020-10-13 15:21:09 -0400

    Added append dv to iv2

--- VectorTester.cpp
+++ VectorTester.cpp
@@ -49,6 +49,33 @@ int main()
    std::cout << "DoubleVector:" << std::endl;
    std::cout << "-------------" << std::endl;
 
+   std::cout  << "Size: " << dv.size() << std::endl;
+   dv.put(12.1);
+   dv.put(13.1);
+   dv.put(14.1);
+   std::cout << "Size: " << dv.size() << " [3]" << std::endl;
+
+   std::cout << "Before any changes" << std::endl;
+   for(int i=0; i < dv.size(); i++) {
+       std::cout << dv.get(i) << std::endl;
+   }
+
+   dv.put(13.5, 1);
+   std::cout << "After putting 13.5 at index 1" << std::endl;
+   for(int i=0; i < dv.size(); i++) {
+       std::cout << dv.get(i) << std::endl;
+   }
+   dv.put(18.5, 5);
+   std::cout << "After putting 18.5 at index 5" << std::endl;
+   for(int i=0; i < dv.size(); i++) {
+       std::cout << dv.get(i) << std::endl;
+   }
+   dv.put(12.5, -1);
+   std::cout << "After putting 12.5 at index -1" << std::endl;
+   for(int i=0; i < dv.size(); i++) {
+       std::cout << dv.get(i) << std::endl;
+   }
+
    //-------------------------------------------------------------------------
 
    // test IntegerVector: put, get, size, out_of_range
@@ -56,31 +83,31 @@ int main()
    std::cout << "--------------" << std::endl;
    std::cout << "IntegerVector:" << std::endl;
    std::cout << "--------------" << std::endl;
-    std::cout  << "Size: " << iv.size() << std::endl;
-    iv.put(65);
-    iv.put(66);
-    iv.put(67);
-    std::cout << "Size: " << iv.size() << " [3]" << std::endl;
-    
-    std::cout << "Before any changes" << std::endl;
-    for(int i=0; i < iv.size(); i++) {
-        std::cout << iv.get(i) << std::endl;
+   std::cout  << "Size: " << iv.size() << std::endl;
+   iv.put(65);
+   iv.put(66);
+   iv.put(67);
+   std::cout << "Size: " << iv.size() << " [3]" << std::endl;
+
+   std::cout << "Before any changes" << std::endl;
+   for(int i=0; i < iv.size(); i++) {
+       std::cout << iv.get(i) << std::endl;
     }
-    
-    iv.put(68, 2);
-    std::cout << "After putting 68 at index 2" << std::endl;
-    for(int i=0; i < iv.size(); i++) {
-        std::cout << iv.get(i) << std::endl;
+
+   iv.put(68, 2);
+   std::cout << "After putting 68 at index 2" << std::endl;
+   for(int i=0; i < iv.size(); i++) {
+       std::cout << iv.get(i) << std::endl;
     }
-    iv.put(69, 10);
-    std::cout << "After putting 69 at index 10" << std::endl;
-    for(int i=0; i < iv.size(); i++) {
-        std::cout << iv.get(i) << std::endl;
+   iv.put(69, 10);
+   std::cout << "After putting 69 at index 10" << std::endl;
+   for(int i=0; i < iv.size(); i++) {
+       std::cout << iv.get(i) << std::endl;
     }
-    iv.put(70, -1);
-    std::cout << "After putting 70 at index -1" << std::endl;
-    for(int i=0; i < iv.size(); i++) {
-        std::cout << iv.get(i) << std::endl;
+   iv.put(70, -1);
+   std::cout << "After putting 70 at index -1" << std::endl;
+   for(int i=0; i < iv.size(); i++) {
+       std::cout << iv.get(i) << std::endl;
     }
 
    //-------------------------------------------------------------------------
@@ -106,22 +133,31 @@ int main()
    std::cout << "appended-to DoubleVector:" << std::endl;
    std::cout << "-------------------------" << std::endl;
 
+   dv2.appendIntegerVector(iv);
+   for(int i = 0; i < dv2.size(); i++)
+      std::cout << dv2.get(i) << std::endl;
+
    //-------------------------------------------------------------------------
 
-   // using empty IntegerVector, test appending cv & dv from above 
+   // using empty IntegerVector, test appending cv & dv from above
    IntegerVector iv2;
 
    std::cout << std::endl;
    std::cout << "--------------------------" << std::endl;
    std::cout << "appended-to IntegerVector:" << std::endl;
    std::cout << "--------------------------" << std::endl;
-    iv2.appendCharacterVector(cv);
-    std::cout << "After appending cv" << std::endl;
-    for(int i=0; i < iv2.size(); i++) {
-        std::cout << iv2.get(i) << std::endl;
+   iv2.appendCharacterVector(cv);
+   std::cout << "After appending cv" << std::endl;
+   for(int i=0; i < iv2.size(); i++) {
+       std::cout << iv2.get(i) << std::endl;
     }
-    
-    
+   iv2.append(dv);
+   std::cout << "After apending dv" << std::endl;
+   for(int i=0; i < iv2.size(); i++) {
+       std::cout << iv2.get(i) << std::endl;
+    }
+
+
 
    //-------------------------------------------------------------------------
 
